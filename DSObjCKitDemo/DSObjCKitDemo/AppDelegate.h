7e1e4e3c9e48792a99e87beb3ccc93f5fbc5577e
dragonsun7 <dragonsun7@163.com>
2017-12-31 21:48:19 +0800

    Initial commit

---
+++ DSObjCKitDemo/DSObjCKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DSObjCKitDemo
+//
+//  Created by Dragon Sun on 2017/12/31.
+//  Copyright © 2017 Dragon Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
